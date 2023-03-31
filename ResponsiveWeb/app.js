58c2bc777c4c158ec9b0cc976e45713e41876e42
Ivan <75530916+RaveZ@users.noreply.github.com>
2023-03-31 07:30:09 +0700

    update

--- ResponsiveWeb/app.js
+++ ResponsiveWeb/app.js
@@ -2,7 +2,16 @@ var title = document.querySelector('.title');
 var courseFeatureElements = document.querySelectorAll('.course-feature');
 var button = document.querySelector('button');
 
-navigator.serviceWorker.register('/sw.js');
+navigator.serviceWorker.register('sw.js');
+
+var deferredPrompt;
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
+
 
 function animate() {
   title.classList.remove('animate-in');
