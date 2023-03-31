5cb5e2e7139ff41f8d34299f4a9fc3c1803fc635
Ivan <75530916+RaveZ@users.noreply.github.com>
2023-03-31 08:23:04 +0700

    update

--- portofolio/app.js
+++ portofolio/app.js
@@ -1,2 +1,12 @@
-
 navigator.serviceWorker.register('sw.js');
+
+
+
+var deferredPrompt;
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
+
